2a24a92574e9a790d7c4d6bf02d4a1f6c2f47107
Luka Dornhecker <luka.dornhecker@gmail.com>
2015-09-16 20:37:46 +0200

    fetch docker top results

--- src/hdtop/main.go
+++ src/hdtop/main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/fsouza/go-dockerclient"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	var (
+		client *docker.Client
+		err    error
+	)
+
+	if client, err = docker.NewClientFromEnv(); err != nil {
+		fmt.Println("Could not connect to docker daemon:", err)
+		os.Exit(1)
+	}
+
+	topResult, _ := client.TopContainer(os.Args[1], strings.Join(os.Args[2:], " "))
+	fmt.Printf("%#v", topResult)
 }
